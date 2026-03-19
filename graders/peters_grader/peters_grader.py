c859e0eb1249b507d87d9a3adebe5a6863acedf3
Peter Jausovec <peter.jausovec@solo.io>
2026-03-18 17:54:42 -0700

    my test grader
    Signed-off-by: Peter Jausovec <peter.jausovec@solo.io>

---
+++ graders/peters_grader/peters_grader.py
@@ -0,0 +1,17 @@
+"""Custom grader: peters_grader
+
+Usage in eval_config.yaml:
+
+    metrics:
+      - name: peters_grader
+        type: code
+        path: ./peters_grader/peters_grader.py
+        threshold: 0.5
+"""
+
+from agentevals_grader_sdk import grader, EvalInput, EvalResult
+
+
+@grader
+def peters_grader(input: EvalInput) -> EvalResult:
+    return EvalResult(score=0.123, details="All good")
